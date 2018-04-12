55be4b9b585853027019f2c7c56be78390c9ef3d
aina-esokia <asr@esokia-webagency.com>
2018-04-12 17:26:04 +0400

    silex2 dependencies

--- src/QueueProvider/QueueServiceProvider.php
+++ src/QueueProvider/QueueServiceProvider.php
@@ -3,10 +3,12 @@
 namespace Alchemy\QueueProvider;
 
 use Alchemy\Queue\MessageQueueRegistry;
+use Pimple\Container;
+use Silex\Api\BootableProviderInterface;
 use Silex\Application;
-use Silex\ServiceProviderInterface;
+use Pimple\ServiceProviderInterface;
 
-class QueueServiceProvider implements ServiceProviderInterface
+class QueueServiceProvider implements ServiceProviderInterface, BootableProviderInterface
 {
 
     /**
@@ -15,11 +17,11 @@ class QueueServiceProvider implements ServiceProviderInterface
      * This method should only be used to configure services and parameters.
      * It should not get services.
      */
-    public function register(Application $app)
+    public function register(Container $app)
     {
-        $app['alchemy_queues.registry'] = $app->share(function (Application $app) {
+        $app['alchemy_queues.registry'] = function (Application $app) {
             return new MessageQueueRegistry();
-        });
+        };
     }
 
     /**
